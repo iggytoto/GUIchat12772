c79e760f88e9416233607cba712225d15a07fb66
Vlalden <vladlen@vozhzhaev.ru>
2023-11-27 19:11:36 +0300

    Добавили поддержку JSON

--- src/main/java/com/example/gui_chat12772/HelloController.java
+++ src/main/java/com/example/gui_chat12772/HelloController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -19,30 +22,40 @@ public class HelloController {
     private Button sendBtn;
     @FXML
     private Button connectBtn;
-
+    private DataOutputStream out;
 
     @FXML
     protected void onSendHandler(){
-        String msg = textField.getText();
-        messageBox.appendText(msg+"\n");
-        textField.clear();
+        try {
+            String msg = textField.getText();
+            messageBox.appendText(msg+"\n");
+            textField.clear();
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     @FXML
     protected void onConnect(){
         try {
             Socket socket = new Socket("127.0.0.1", 9123);
             DataInputStream is = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            out = new DataOutputStream(socket.getOutputStream());
             sendBtn.setDisable(false);
             connectBtn.setDisable(true);
             Thread thread = new Thread(()->{
                 try {
                     while (true){
                         String response = is.readUTF();
-                        messageBox.appendText(response+"\n");
+                        JSONParser jsonParser = new JSONParser();
+                        JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
+                        String msg = jsonObject.get("msg").toString();
+                        messageBox.appendText(msg+"\n");
                     }
                 }catch (IOException e){
                     System.out.println("Потеряно соединение с сервером");
+                }catch (ParseException e){
+                    e.printStackTrace();
                 }
             });
             thread.start();
