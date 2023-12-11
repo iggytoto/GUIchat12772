79b61c46bc914804693eae0e31f8542dbcc05d9a
Vlalden <vladlen@vozhzhaev.ru>
2023-12-11 19:10:57 +0300

    addUserBtn method

--- src/main/java/com/example/gui_chat12772/HelloController.java
+++ src/main/java/com/example/gui_chat12772/HelloController.java
@@ -36,19 +36,17 @@ public class HelloController {
     @FXML
     protected void onSendHandler() {
         try {
-            JSONObject jsonObject = new JSONObject();
-            String msg = textField.getText();
-            jsonObject.put("msg", msg);
-            jsonObject.put("to_id", toId);
-            messageBox.appendText("me>: "+msg+"\n");
-            textField.clear();
-            out.writeUTF(jsonObject.toJSONString());
+            JSONObject jsonObject = new JSONObject(); // Создали пустой JSON
+            String msg = textField.getText(); // Прочитали что ввёл пользователь
+            jsonObject.put("msg", msg); // Добавили сообщение пользователя в JSON
+            jsonObject.put("to_id", toId); // Добавили получателя сообщения в JSON
+            messageBox.appendText("me>: "+msg+"\n"); // Печатаем на экран своё же сообщение
+            textField.clear(); // Очищаем поле в которое пользователь вводил сообщение
+            out.writeUTF(jsonObject.toJSONString()); // Передаём JSON на сервер
             textField.requestFocus();   // переведем фокус в поле ввода
-        } catch (NullPointerException e) {
-//            throw new RuntimeException(e);
+        } catch (IOException e) {
             messageBox.appendText("Нет  подключения к серверу...\n");
             textField.clear();
-        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
@@ -56,38 +54,29 @@ public class HelloController {
     protected void onConnect(){
         try {
             Socket socket = new Socket("127.0.0.1", 9123);
-            DataInputStream is = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            sendBtn.setDisable(false);
-
-            textField.requestFocus();
-            thread = new Thread(()->{
+            DataInputStream is = new DataInputStream(socket.getInputStream()); // Поток ввода
+            out = new DataOutputStream(socket.getOutputStream()); // Поток вывода
+            sendBtn.setDisable(false); // Активируем кнопку отправки (до подключения она выключена)
+            textField.requestFocus(); // Ставим каретку в поле ввода сообщения
+            thread = new Thread(()->{ // Поток для приёма сообщений с сервера
                 try {
                     while (true){
-                        String response = is.readUTF();
-                        JSONParser jsonParser = new JSONParser();
-                        JSONObject jsonObject = (JSONObject) jsonParser.parse(response);
-                        if(jsonObject.containsKey("msg")){
-                            String msg = jsonObject.get("msg").toString();
-                            messageBox.appendText(msg + "\n");
-                        } else if (jsonObject.containsKey("onlineUsers")) {
-                            JSONArray jsonArray = (JSONArray) jsonObject.get("onlineUsers");
+                        String response = is.readUTF(); // Ждём сообщение от сервера (в формате JSON)
+                        JSONParser jsonParser = new JSONParser(); // Парсер для JSON
+                        JSONObject jsonObject = (JSONObject) jsonParser.parse(response); // Парсим JSON
+                        if(jsonObject.containsKey("msg")){ // Проверяем что сервер прислал msg
+                            String msg = jsonObject.get("msg").toString(); // Достаём сообщение
+                            messageBox.appendText(msg + "\n"); // Печатаем на экран
+                        } else if (jsonObject.containsKey("onlineUsers")) { // Проверяем что сервер прислал onlineUsers
+                            JSONArray jsonArray = (JSONArray) jsonObject.get("onlineUsers"); // Достаём список
                             Platform.runLater(()->{
-                                usersBox.getChildren().clear();
-                                jsonArray.forEach(user->{
+                                usersBox.getChildren().clear(); // Очищаем предыдущий список пользователей
+                                addUserButton("Общий чат", 0);
+                                jsonArray.forEach(user->{ // Перебираем список пользователей
                                     JSONObject jsonUser = (JSONObject) user; // уточняем, что user это JSONObject
-                                    Button userBtn = new Button();
-                                    userBtn.setText(jsonUser.get("name").toString());
-                                    userBtn.setPrefWidth(200);
-                                    userBtn.setOnAction((e)->{
-                                        toId = Integer.parseInt(jsonUser.get("id").toString()); // Кто получатель
-                                        messageBox.clear();
-                                    });
-                                    usersBox.getChildren().add(userBtn);
-                                    //listBox.appendText(user + "\n");
+                                    addUserButton(jsonUser.get("name").toString(), Integer.parseInt(jsonUser.get("id").toString()));
                                 });
                             });
-                            //listBox.clear();
                         }
                     }
                 }catch (IOException e){
@@ -100,5 +89,16 @@ public class HelloController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void addUserButton(String name, int id){
+        Button userBtn = new Button(); // Создаём кнопку
+        userBtn.setText(name); // Пишем на кнопке имя пользователя
+        userBtn.setPrefWidth(200); // Устанавливаем ширину кнопки
+        userBtn.setOnAction((e)->{ // Устанавливаем функцию-обработчик при клике на кнопку
+            toId = id; // Кто получатель
+            messageBox.clear(); // Очищаем сообщения чата
+        });
+        usersBox.getChildren().add(userBtn); // Добавляем кнопку на экран
     }
 }
